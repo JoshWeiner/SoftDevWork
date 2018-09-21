dd16984b603f8ce7dbdc5ca993abb76d9bad57d3
JoshWeiner <joshuaweinernyc@gmail.com>
2018-09-21 12:30:57 -0400

    adding csv

--- 09_21/app.py
+++ 09_21/app.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template
+import csv,random
 app = Flask(__name__)
+
+occDict = {}
 
 coll = [0,1,1,2,3,5,8]
 
@@ -8,6 +11,21 @@ def home():
     return render_template("template.html",
                         coll=coll)
 
+@app.route("/occupations")
+def occupado():
+    with open('occupations.csv') as infile:
+    reader = csv.DictReader(infile)
+    for row in reader:
+         occDict[ row['Job Class'] ] = row['Percentage']
+    occDict.pop('Total')
+    for i in occDict.keys():
+        occDict[i] = eval(occDict[i])
+    for job in occDict.keys():
+        for numTimes in range(int(D[job] * 10)):
+            #Need to convert percentages to integers accurately to use the range() function,
+            weighted_array.append(job)
+            #populate an array of ~1000 values, because of the use of range
+            #array populated by percentages
 if __name__ == "__main__":
     app.debug=True
     app.run()
