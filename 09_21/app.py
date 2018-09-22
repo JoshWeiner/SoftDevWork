b42af39832c8e399bffaa390fb787e2e70d9d112
Josh Weiner <joshuaweinernyc@gmail.com>
2018-09-22 18:34:48 -0400

    completed homework, fixed issued Bo ran into with Jinja

--- 09_21/app.py
+++ 09_21/app.py
@@ -3,6 +3,7 @@ import csv,random
 app = Flask(__name__)
 
 occDict = {}
+occList = []
 
 coll = [0,1,1,2,3,5,8]
 
@@ -14,18 +15,20 @@ def home():
 @app.route("/occupations")
 def occupado():
     with open('occupations.csv') as infile:
-    reader = csv.DictReader(infile)
-    for row in reader:
-         occDict[ row['Job Class'] ] = row['Percentage']
-    occDict.pop('Total')
+        reader = csv.DictReader(infile)
+        for row in reader:
+             occDict[ row['Job Class'] ] = row['Percentage']
+        occDict.pop('Total')
     for i in occDict.keys():
         occDict[i] = eval(occDict[i])
     for job in occDict.keys():
-        for numTimes in range(int(D[job] * 10)):
+        for numTimes in range(int(occDict[job] * 10)):
             #Need to convert percentages to integers accurately to use the range() function,
-            weighted_array.append(job)
+            occList.append(job)
             #populate an array of ~1000 values, because of the use of range
             #array populated by percentages
+    return render_template("template.html", occ=occDict, randocc=random.choice(occList))
+
 if __name__ == "__main__":
     app.debug=True
     app.run()
